6ef07a2ba7a523eb78d29b4369806a008fdb0466
awalmubarak <awalmubarak6@gmail.com>
2018-05-25 23:13:44 +0000

    intial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+hbs.registerHelper('getCurrentYear', ()=>new Date().getFullYear());
+hbs.registerHelper('screamIt', (text)=>text.toUpperCase());
+
+app.use((req,res,next)=>{
+  var now  = new Date().toString();
+  var log = `${now}: Method:${req.method} ${req.url}`;
+  fs.appendFileSync('server.log', log + '\n');
+  console.log(log);
+  next();
+});
+
+app.use((req,res,next)=>{
+  res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res)=>{
+  var user = {
+    name: 'Awal',
+    age: 21,
+    networth: 'timeout',
+    company: 'anditer'
+  };
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my node app',
+    user
+  })
+});
+
+app.get('/about', (req, res)=>{
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+
+app.get('/bad', (req, res)=>{
+  res.send({
+    errorMessage: 'Something went wrong'
+  });
+});
+
+
+app.listen(3000, ()=>{
+  console.log('Server running on port 3000');
+});
